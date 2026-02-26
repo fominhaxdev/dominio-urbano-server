60ca1265e00612ba90e24b8499a563a390f50b86
fominhaxdev <fominhaxdev@gmail.com>
2026-02-26 11:27:45 -0300

    Tentativa 4 db-config.js

--- db-config.js
+++ db-config.js
@@ -2,13 +2,14 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'localhost',
+    host: 'svr728.hostinger.com', // Host correto da Hostinger
     user: 'u131408987_DU',
-    password: 'Eu991843135@', // 🔴 COLOQUE A SENHA DO BANCO AQUI
+    password: 'Eu991843135@',
     database: 'u131408987_DominioUrbano',
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    port: 3306 // Porta padrão do MySQL
 });
 
 module.exports = pool.promise();
